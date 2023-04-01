404e076d1c825be134d965dc1685c6e8fcdcd4e6
JDavid <striker.one22@gmail.com>
2023-04-01 16:54:03 -0600

    Agregando Index

---
+++ index.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Prueba Git</title>
+
+    <!-- Librerías -->
+    <link rel="stylesheet" href="libs/bootstrap/css/bootstrap.min.css">
+</head>
+
+<body class="bg-dark">
+    <div class="container">
+
+        <div class="row-fluid text-white">
+            <div class="col-md-12">
+                <h3>Prueba de Git/GitHub</h3>
+                <p class="text-secondary">Lorem ipsum dolor sit amet consectetur, adipisicing elit. Fuga ex dicta aspernatur expedita delectus laborum at, quaerat odit voluptas alias eum id sapiente velit natus impedit repudiandae nobis nihil quisquam.</p>
+            </div>
+        </div>
+
+
+        <div class="row-fluid">
+
+            <!-- Espacio para David -->
+            <div class="col-md-4">
+                <div class="card text-center bg-dark">
+                    <div class="card-header bg-warning">
+                        David
+                    </div>
+                    <div class="card-body bg-secondary">
+                        <h5 class="card-title">Sobre mi ...</h5>
+                        <p class="card-text">Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa, neque saepe. Commodi, vero dolores assumenda expedita doloremque illum debitis saepe iusto, sit unde reprehenderit. Numquam architecto soluta illo temporibus repellat!</p>
+                        <a href="#" class="btn btn-primary">Like</a>
+                    </div>
+                    <div class="card-footer text-muted">
+                        01/Abr/23 15:59
+                    </div>
+                </div>
+            </div>
+            <!-- Fin espacio para David -->
+
+        </div>
+    </div>
+
+
+    <script src="libs/jquery.js"></script>
+    <script src="libs/bootstrap/js/bootstrap.min.js"></script>
+</body>
+
+</html>
